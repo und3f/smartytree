d4cb9201f8c6fb6e4ce9ebf95cf578cb655110db
Sergii Zasenko <sergii@zasenko.name>
2022-06-12 19:56:40 +0300

    fill init

---
+++ src/main/java/name/zasenko/smarty/snake/strategy/Fill.java
@@ -0,0 +1,13 @@
+package name.zasenko.smarty.snake.strategy;
+
+import name.zasenko.smarty.snake.Context;
+import name.zasenko.smarty.snake.Direction;
+
+public class Fill extends Cycle {
+
+    @Override
+    public Direction findMove(Context ctx) {
+        return super.findMove(ctx);
+    }
+
+}
---
+++ src/test/java/name/zasenko/smarty/snake/FillTest.java
@@ -0,0 +1,22 @@
+package name.zasenko.smarty.snake;
+
+import name.zasenko.smarty.BaseUnitTestHelper;
+import name.zasenko.smarty.snake.strategy.Fill;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FillTest extends BaseUnitTestHelper {
+
+    @Disabled
+    @Test
+    void testFill() throws IOException {
+        GameState gameState = readState("fill/fill-test");
+
+        assertEquals(Direction.right, new Context(gameState).findMove(new Fill()));
+    }
+
+}
